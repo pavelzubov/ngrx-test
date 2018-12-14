02c8942bb3d5299ecf54e7659affd1d261c68895
pavelzubov <pasha224@yandex.ru>
2018-12-14 14:55:23 +0300

    Change files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Column} from './table/column';
+import {ProgramDetails, ProgramsList, ProgramsService} from './api';
+import {select, Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {GetAll} from './store/actions/programs.actions';
+import {map} from 'rxjs/operators';
+import {GetStatisticsRequest} from './store/actions/platform.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private store: Store<{ programs: any[] }>, public programsService: ProgramsService) {
+    // this.programs = store.pipe(select('programs'), map(res => res.programs));
+  }
+
+  programs: Observable<any[]>;
   title = 'angular-test';
+  columns: Column[] = [
+    {name: 'currency', label: 'currency'},
+    {name: 'level', label: 'level'},
+    {name: 'periodDuration', label: 'periodDuration'},
+    {name: 'periodStarts', label: 'periodStarts'},
+    {name: 'periodEnds', label: 'periodEnds'},
+    {name: 'availableInvestment', label: 'availableInvestment'}
+  ];
+  data: ProgramsList;
+
+  ngOnInit() {
+    this.store.dispatch(new GetAll());
+    const interval = setInterval(_ => {
+      this.store.dispatch(new GetStatisticsRequest());
+    }, 5000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ApiModule} from './api';
+import {TableComponent} from './table/table/table.component';
+import {StoreModule} from '@ngrx/store';
+import {programsReducer} from './store/reducers/programs.reducer';
+import {InsideBlockComponent} from './inside-block/inside-block.component';
+import {EffectsModule} from '@ngrx/effects';
+import {ProgramsEffects} from './store/effects/programs.effects';
+import {platformReducer} from './store/reducers/platform.reducer';
+import {PlatformEffects} from './store/effects/platform.effects';
+import { StatisticComponent } from './statistic/statistic.component';
+import { StatisticViewComponent } from './statistic/statistic-view/statistic-view.component';
+import {reducers} from './store/reducers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    InsideBlockComponent,
+    StatisticComponent,
+    StatisticViewComponent
   ],
   imports: [
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([ProgramsEffects, PlatformEffects]),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
