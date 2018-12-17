9e1cecc8d10e88b63c6aae80760e51c7e8da6168
pavelzubov <pasha224@yandex.ru>
2018-12-17 21:19:19 +0300

    Refactor stream

--- src/app/store/effects/socket.effects.ts
+++ src/app/store/effects/socket.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Observable, of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
 import { ActionTypes, EffectAction } from '../actions/socket.actions';
 import { ChainElement, WebsocketService } from '../../services/websocket.service';
 import { TradeActionTypes } from '../../modules/symbol-trade/symbol-trade.actions';
@@ -16,35 +16,33 @@ export class SocketEffects {
     @Effect()
     Chain$: Observable<Action> = this.actions$.pipe(
         ofType(ActionTypes.GetChainSocketRequest),
-        mergeMap((action: EffectAction) => {
-            return this.websocketService
-                .chainSocket(<ChainElement>{
-                    symbol: action.payload.symbol,
-                    levels: action.payload.levels,
-                    interval: action.payload.interval
-                })
-                .pipe(
-                    map(data => {
-                        const method = data.stream.split('@')[1];
-                        switch (method) {
-                            case 'trade':
-                                return {
-                                    type: TradeActionTypes.GetSymbolTradeSocketSuccess,
-                                    payload: data.data
-                                };
-                            case `depth${action.payload.levels}`:
-                                return {
-                                    type: DepthActionTypes.GetSymbolDepthSocketSuccess,
-                                    payload: data.data
-                                };
-                            case 'arr':
-                                return {
-                                    type: TrackerArrActionTypes.GetTrackerArrSocketSuccess,
-                                    payload: data.data
-                                };
-                        }
-                    })
-                );
+        switchMap((action: EffectAction) =>
+            this.websocketService.chainSocket(<ChainElement>{
+                symbol: action.payload.symbol,
+                levels: action.payload.levels,
+                interval: action.payload.interval
+            })
+        ),
+        map(data => {
+            const method = data.stream.split('@')[1];
+            switch (method) {
+                case 'trade':
+                    return {
+                        type: TradeActionTypes.GetSymbolTradeSocketSuccess,
+                        payload: data.data
+                    };
+                case 'arr':
+                    return {
+                        type: TrackerArrActionTypes.GetTrackerArrSocketSuccess,
+                        payload: data.data
+                    };
+                case `depth`:
+                default:
+                    return {
+                        type: DepthActionTypes.GetSymbolDepthSocketSuccess,
+                        payload: data.data
+                    };
+            }
         })
     );
 
