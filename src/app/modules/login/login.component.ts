263c7ccb1438564c79291d92a8e3b3978d1cc987
pavelzubov <pasha224@yandex.ru>
2018-12-23 22:43:21 +0300

    Add add async validation

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+    AbstractControl,
+    AsyncValidatorFn,
+    FormControl,
+    FormGroup,
+    Validators
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
     selector: 'app-login',
@@ -11,7 +19,7 @@ export class LoginComponent implements OnInit {
     private login: string;
     private password: string;
     private loginForm = new FormGroup({
-        login: new FormControl('', Validators.required),
+        login: new FormControl('', Validators.required, this.validateName()),
         password: new FormControl('', Validators.required)
     });
     constructor(public loginService: LoginService) {}
@@ -23,5 +31,24 @@ export class LoginComponent implements OnInit {
         this.loginService
             .login(this.login, this.password)
             .subscribe(res => console.log(res));
+    }
+    validateName(): AsyncValidatorFn {
+        /** Эмуляция запроса на сервер */
+        return (control: AbstractControl): Observable<any> =>
+            new Observable<any>(observer => {
+                const user = '123' === control.value;
+                /** если пользователь есть в массиве, то возвращаем ошибку */
+                if (user) {
+                    // console.log('Пользователь с таким именем уже существует');
+                    observer.next({
+                        nameError: 'Пользователь с таким именем уже существует'
+                    });
+                    observer.complete();
+                }
+
+                /** Если пользователя нет, то валидация успешна */
+                observer.next(null);
+                observer.complete();
+            }).pipe(delay(1000));
     }
 }
