2996d0bbd174fb14a34b48ff87dbf16b768beb0f
pavelzubov <pasha224@yandex.ru>
2019-01-04 00:03:46 +0300

    Update files

--- src/app/components/trade-form/trade-form.component.ts
+++ src/app/components/trade-form/trade-form.component.ts
@@ -33,20 +33,21 @@ export class TradeFormComponent implements OnInit, OnChanges {
     private tradeForm = new FormGroup({
         price: new FormControl(this.price, [Validators.required]),
         quantity: new FormControl(null, [Validators.required]),
-        total: new FormControl(null, [Validators.required, Validators.min(0.001)])
+        total: new FormControl(0.0012, [Validators.required, Validators.min(0.001)])
     });
     constructor() {}
 
     ngOnInit() {}
+
     ngOnChanges(changes) {
         this.tradeForm.get('price').setValue(this.price);
+        this.totalChange();
     }
 
     public quantityChange(): void {
         const price = this.tradeForm.get('price');
         const total = this.tradeForm.get('total');
         const quantity = this.tradeForm.get('quantity');
-        console.log(this.tradeForm.value);
         total.setValue(quantity.value * price.value);
     }
     public totalChange(): void {
