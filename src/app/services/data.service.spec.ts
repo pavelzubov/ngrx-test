940a88372a00da0bd10889fd9052a5751aea0427
pavelzubov <pasha224@yandex.ru>
2019-01-17 21:03:26 +0300

    Update files

--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import { Store, StoreModule } from '@ngrx/store';
+import { reducers } from '../store/reducers';
 
 describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    let store: Store<any>;
+    beforeEach(() => {
+        TestBed.configureTestingModule({ imports: [StoreModule.forRoot(reducers)] });
+        store = TestBed.get(Store);
+        spyOn(store, 'dispatch').and.callThrough();
+    });
 
-  it('should be created', () => {
-    const service: DataService = TestBed.get(DataService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: DataService = TestBed.get(DataService);
+        expect(service).toBeTruthy();
+    });
 });
--- src/app/store_/actions/programs.ts
+++
@@ -1,9 +0,0 @@
-import { Action } from '@ngrx/store';
-import {ProgramsList} from '../../api';
-export const GET_ALL = '[Films] Get all';
-export class GetAll implements Action {
-  readonly type = GET_ALL;
-  constructor(public payload: ProgramsList) { }
-}
-
-export type Action = GetAll;
